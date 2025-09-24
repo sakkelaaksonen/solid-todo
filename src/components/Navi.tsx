b4ba5248f669ad4cae503b30bedfb06a4ffe155e
Sakari Laaksonen <sakke.laaksonen@gmail.com>
2025-09-24 19:15:06 +0300

    Better drawer button for large screens

--- src/components/Navi.tsx
+++ src/components/Navi.tsx
@@ -16,20 +16,18 @@ const Navi: Component<Props> = (props) => {
 
   return (
     <nav class="navbar bg-secondary-content border-transparent border-b-primary border-2 shadow-sm mb-2 sticky top-0 z-10 min-h-24">
-
-
-
       <ListSelector store={props.store} actions={props.actions} />
-
       <div class="flex-1"></div>
       <div class="flex-none pe-4">
         <button
           title="Manage Todo Lists"
-          class="btn btn-square btn-ghost hover:text-primary focus:text-primary"
+          class="btn max-md:btn-square btn-primary"
           aria-label="Manage Todo Lists" /* Adds an accessible label */
           onClick={handleDrawerToggle}
         >
+
           <IconBars />
+          <span class="hidden md:inline">Manage Todo Lists</span>
         </button>
       </div>
 
